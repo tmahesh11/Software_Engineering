85668e3e270d4439128f9b4700b5b534121c79e0
Mahesh <timsinamahesh11@gmail.com>
2023-02-06 19:18:44 +0100

    SE_258971_Assignment1

--- SE_258971_Assignment1/SE_258971_Assignment1/Form1.Designer.cs
+++ SE_258971_Assignment1/SE_258971_Assignment1/Form1.Designer.cs
@@ -29,13 +29,13 @@
         private void InitializeComponent()
         {
             this.buttonSampling = new System.Windows.Forms.Button();
-            this.textBoxSampling = new System.Windows.Forms.TextBox();
             this.labelSampling = new System.Windows.Forms.Label();
             this.groupBoxSampling = new System.Windows.Forms.GroupBox();
+            this.textBoxNextSamplingTime = new System.Windows.Forms.TextBox();
             this.buttonLogging = new System.Windows.Forms.Button();
-            this.textBoxLogging = new System.Windows.Forms.TextBox();
             this.labelLogging = new System.Windows.Forms.Label();
             this.groupBoxLogging = new System.Windows.Forms.GroupBox();
+            this.textBoxNextLoggingTime = new System.Windows.Forms.TextBox();
             this.textBoxShow = new System.Windows.Forms.TextBox();
             this.groupBoxShow = new System.Windows.Forms.GroupBox();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
@@ -50,7 +50,7 @@
             // 
             // buttonSampling
             // 
-            this.buttonSampling.Location = new System.Drawing.Point(81, 126);
+            this.buttonSampling.Location = new System.Drawing.Point(6, 96);
             this.buttonSampling.Name = "buttonSampling";
             this.buttonSampling.Size = new System.Drawing.Size(75, 23);
             this.buttonSampling.TabIndex = 0;
@@ -58,18 +58,10 @@
             this.buttonSampling.UseVisualStyleBackColor = true;
             this.buttonSampling.Click += new System.EventHandler(this.buttonSampling_Click);
             // 
-            // textBoxSampling
-            // 
-            this.textBoxSampling.Location = new System.Drawing.Point(215, 59);
-            this.textBoxSampling.Name = "textBoxSampling";
-            this.textBoxSampling.Size = new System.Drawing.Size(113, 23);
-            this.textBoxSampling.TabIndex = 1;
-            this.textBoxSampling.TextChanged += new System.EventHandler(this.textBoxSampling_TextChanged);
-            // 
             // labelSampling
             // 
             this.labelSampling.AutoSize = true;
-            this.labelSampling.Location = new System.Drawing.Point(16, 30);
+            this.labelSampling.Location = new System.Drawing.Point(6, 28);
             this.labelSampling.Name = "labelSampling";
             this.labelSampling.Size = new System.Drawing.Size(114, 15);
             this.labelSampling.TabIndex = 2;
@@ -78,18 +70,28 @@
             // 
             // groupBoxSampling
             // 
+            this.groupBoxSampling.Controls.Add(this.buttonSampling);
+            this.groupBoxSampling.Controls.Add(this.textBoxNextSamplingTime);
             this.groupBoxSampling.Controls.Add(this.labelSampling);
             this.groupBoxSampling.Location = new System.Drawing.Point(60, 37);
             this.groupBoxSampling.Name = "groupBoxSampling";
-            this.groupBoxSampling.Size = new System.Drawing.Size(285, 156);
+            this.groupBoxSampling.Size = new System.Drawing.Size(332, 156);
             this.groupBoxSampling.TabIndex = 3;
             this.groupBoxSampling.TabStop = false;
             this.groupBoxSampling.Text = "Sampling";
             this.groupBoxSampling.Enter += new System.EventHandler(this.groupBoxSampling_Enter);
             // 
+            // textBoxNextSamplingTime
+            // 
+            this.textBoxNextSamplingTime.Location = new System.Drawing.Point(149, 25);
+            this.textBoxNextSamplingTime.Name = "textBoxNextSamplingTime";
+            this.textBoxNextSamplingTime.Size = new System.Drawing.Size(161, 23);
+            this.textBoxNextSamplingTime.TabIndex = 3;
+            this.textBoxNextSamplingTime.TextChanged += new System.EventHandler(this.textBoxNextSamplingTime_TextChanged);
+            // 
             // buttonLogging
             // 
-            this.buttonLogging.Location = new System.Drawing.Point(21, 95);
+            this.buttonLogging.Location = new System.Drawing.Point(0, 131);
             this.buttonLogging.Name = "buttonLogging";
             this.buttonLogging.Size = new System.Drawing.Size(103, 23);
             this.buttonLogging.TabIndex = 4;
@@ -97,51 +99,52 @@
             this.buttonLogging.UseVisualStyleBackColor = true;
             this.buttonLogging.Click += new System.EventHandler(this.buttonLogging_Click);
             // 
-            // textBoxLogging
-            // 
-            this.textBoxLogging.Location = new System.Drawing.Point(155, 37);
-            this.textBoxLogging.Name = "textBoxLogging";
-            this.textBoxLogging.Size = new System.Drawing.Size(113, 23);
-            this.textBoxLogging.TabIndex = 5;
-            this.textBoxLogging.TextChanged += new System.EventHandler(this.textBoxLogging_TextChanged);
-            // 
             // labelLogging
             // 
             this.labelLogging.AutoSize = true;
-            this.labelLogging.Location = new System.Drawing.Point(16, 45);
+            this.labelLogging.Location = new System.Drawing.Point(6, 31);
             this.labelLogging.Name = "labelLogging";
             this.labelLogging.Size = new System.Drawing.Size(108, 15);
             this.labelLogging.TabIndex = 6;
             this.labelLogging.Text = "Next Logging Time";
+            this.labelLogging.Click += new System.EventHandler(this.labelLogging_Click);
             // 
             // groupBoxLogging
             // 
+            this.groupBoxLogging.Controls.Add(this.textBoxNextLoggingTime);
             this.groupBoxLogging.Controls.Add(this.buttonLogging);
-            this.groupBoxLogging.Controls.Add(this.textBoxLogging);
             this.groupBoxLogging.Controls.Add(this.labelLogging);
-            this.groupBoxLogging.Location = new System.Drawing.Point(60, 238);
+            this.groupBoxLogging.Location = new System.Drawing.Point(60, 217);
             this.groupBoxLogging.Name = "groupBoxLogging";
-            this.groupBoxLogging.Size = new System.Drawing.Size(285, 139);
+            this.groupBoxLogging.Size = new System.Drawing.Size(332, 160);
             this.groupBoxLogging.TabIndex = 7;
             this.groupBoxLogging.TabStop = false;
             this.groupBoxLogging.Text = "Logging";
             this.groupBoxLogging.Enter += new System.EventHandler(this.groupBoxLogging_Enter);
+            // 
+            // textBoxNextLoggingTime
+            // 
+            this.textBoxNextLoggingTime.Location = new System.Drawing.Point(149, 23);
+            this.textBoxNextLoggingTime.Name = "textBoxNextLoggingTime";
+            this.textBoxNextLoggingTime.Size = new System.Drawing.Size(161, 23);
+            this.textBoxNextLoggingTime.TabIndex = 7;
+            this.textBoxNextLoggingTime.TextChanged += new System.EventHandler(this.textBoxNextLoggingTime_TextChanged);
             // 
             // textBoxShow
             // 
             this.textBoxShow.Location = new System.Drawing.Point(16, 22);
             this.textBoxShow.Multiline = true;
             this.textBoxShow.Name = "textBoxShow";
-            this.textBoxShow.Size = new System.Drawing.Size(152, 294);
+            this.textBoxShow.Size = new System.Drawing.Size(152, 337);
             this.textBoxShow.TabIndex = 8;
             this.textBoxShow.TextChanged += new System.EventHandler(this.textBoxShow_TextChanged);
             // 
             // groupBoxShow
             // 
             this.groupBoxShow.Controls.Add(this.textBoxShow);
-            this.groupBoxShow.Location = new System.Drawing.Point(425, 40);
+            this.groupBoxShow.Location = new System.Drawing.Point(425, 12);
             this.groupBoxShow.Name = "groupBoxShow";
-            this.groupBoxShow.Size = new System.Drawing.Size(200, 337);
+            this.groupBoxShow.Size = new System.Drawing.Size(200, 365);
             this.groupBoxShow.TabIndex = 9;
             this.groupBoxShow.TabStop = false;
             this.groupBoxShow.Text = "Sensor Values";
@@ -183,8 +186,6 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(703, 398);
-            this.Controls.Add(this.textBoxSampling);
-            this.Controls.Add(this.buttonSampling);
             this.Controls.Add(this.groupBoxSampling);
             this.Controls.Add(this.groupBoxLogging);
             this.Controls.Add(this.groupBoxShow);
@@ -208,11 +209,9 @@
         #endregion
 
         private Button buttonSampling;
-        private TextBox textBoxSampling;
         private Label labelSampling;
         private GroupBox groupBoxSampling;
         private Button buttonLogging;
-        private TextBox textBoxLogging;
         private Label labelLogging;
         private GroupBox groupBoxLogging;
         private TextBox textBoxShow;
@@ -221,5 +220,7 @@
         private ToolStripMenuItem fileToolStripMenuItem;
         private ToolStripMenuItem operationsToolStripMenuItem;
         private ToolStripMenuItem helpToolStripMenuItem;
+        private TextBox textBoxNextSamplingTime;
+        private TextBox textBoxNextLoggingTime;
     }
 }
--- SE_258971_Assignment1/SE_258971_Assignment1/Form1.cs
+++ SE_258971_Assignment1/SE_258971_Assignment1/Form1.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace SE_258971_Assignment1
 {
     public partial class Form1 : Form
@@ -7,22 +9,79 @@ namespace SE_258971_Assignment1
             InitializeComponent();
         }
 
-        private void textBoxSampling_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
         private void buttonSampling_Click(object sender, EventArgs e)
         {
+            // Check for valid sampling time interval
+            int nextSamplingTime = 0;
+            if (!int.TryParse(textBoxNextSamplingTime.Text, out nextSamplingTime))
+            {
+                MessageBox.Show("Invalid Sampling Time Interval!");
+                return;
+            }
 
+            // Create an array of sensors
+            Sensor[] sensors = new Sensor[13];
+            for (int i = 0; i < 9; i++)
+            {
+                sensors[i] = new Sensor(i, false);
+            }
+            for (int i = 9; i < 13; i++)
+            {
+                sensors[i] = new Sensor(i, true);
+            }
+
+            // Read all the sensor values
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 13; i++)
+            {
+                sb.AppendFormat("Sensor {0}: {1}\r\n", sensors[i].GetSensId(), sensors[i].GetValue());
+            }
+
+            // Display the sensor values in the textbox show field
+            textBoxShow.Text = sb.ToString();
+
+            // Update the Next Sampling Time
+            textBoxNextSamplingTime.Text = (nextSamplingTime + 4).ToString();
         }
+
 
         private void buttonLogging_Click(object sender, EventArgs e)
         {
+            // Check for valid logging time interval
+            int nextLoggingTime = 0;
+            if (!int.TryParse(textBoxNextLoggingTime.Text, out nextLoggingTime))
+            {
+                MessageBox.Show("Invalid Logging Time Interval!");
+                return;
+            }
+
+            // Write the last read sensor values to a text file in CSV Type
+            string fileName = string.Format("SensorData_{0}.csv", DateTime.Now.ToString("yyyyMMdd_HHmmss"));
+            string filePath = Path.Combine(Environment.CurrentDirectory, fileName);
+
+            using (StreamWriter sw = new StreamWriter(filePath, true))
+            {
+                sw.WriteLine("Time,Sensor 0,Sensor 1,Sensor 2,Sensor 3,Sensor 4,Sensor 5,Sensor 6,Sensor 7,Sensor 8,Sensor 9,Sensor 10,Sensor 11,Sensor 12");
+                var sensorValues = textBoxShow.Text.Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries);
+                sw.WriteLine("{0}\n {1} {2} {3} {4} {5} {6} {7} {8} {9} {10} {11} {12} {13}",
+                              DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"),
+                              sensorValues[0], sensorValues[1], sensorValues[2], sensorValues[3],
+                              sensorValues[4], sensorValues[5], sensorValues[6], sensorValues[7],
+                              sensorValues[8], sensorValues[9], sensorValues[10], sensorValues[11],
+                              sensorValues[12]);
+            }
+
+
+            // Update the Next Logging Time
+            textBoxNextLoggingTime.Text = (nextLoggingTime + 42).ToString();
+        }
+
+        private void textBoxNextSamplingTime_TextChanged(object sender, EventArgs e)
+        {
 
         }
 
-        private void textBoxLogging_TextChanged(object sender, EventArgs e)
+        private void textBoxNextLoggingTime_TextChanged(object sender, EventArgs e)
         {
 
         }
@@ -33,6 +92,11 @@ namespace SE_258971_Assignment1
         }
 
         private void labelSampling_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelLogging_Click(object sender, EventArgs e)
         {
 
         }
@@ -58,5 +122,6 @@ namespace SE_258971_Assignment1
                                "Input Information", System.Windows.Forms.MessageBoxButtons.OK);
 
         }
+        
     }
 }
